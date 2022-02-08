ada8f4201d06e8755092af13a0df66d0d066baf4
hogehoge <hogehoge@gmail.com>
2022-02-08 14:36:11 +0900

    p280まで作成

--- Assets/Scripts/NejikoController.cs
+++ Assets/Scripts/NejikoController.cs
@@ -4,14 +4,160 @@ using UnityEngine;
 
 public class NejikoController : MonoBehaviour
 {
+    const int MinLane = -2;
+    const int MaxLane = 2;
+    const float LaneWidth = 1f;
+    const int DefaultLife = 3;
+    const float StunDuration = 0.5f;
+
     CharacterController controller;
     Animator animator;
 
     Vector3 moveDirection = Vector3.zero;
+    int targetLane;
+    int life = DefaultLife;
+    float recoverTime = 0.0f;
 
     public float gravity;
     public float speedZ;
+    public float speedX;
     public float speedJump;
+    public float accelerationZ;
+
+    public int Life()
+    {
+        return life;
+    }
+    bool IsStun()
+    {
+        return recoverTime > 0.0f || life <= 0;//recoverTimeに値が入ってるかライフが0か
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();//this.gameObject.GetComponent<CharacterController>();の略
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()//フレーム数によって回数が違う
+    {
+        if (Input.GetKeyDown("left"))//GetKey ボタンを押してる間 GetKeyUp ボタンを離した時 GetKeyDown ボタンを押したときだけ
+        {
+            MoveToLeft();
+        }
+        if (Input.GetKeyDown("right"))
+        {
+            MoveToRight();
+        }
+        if (Input.GetKeyDown("space"))
+        {
+            Jump();
+        }
+
+        if (IsStun())
+        {
+            moveDirection.x = 0.0f;
+            moveDirection.z = 0.0f;
+            recoverTime -= Time.deltaTime;//1秒間をフレーム数で割った数がTime.deltaTime 厳密に言うと1フレームを描画する時間　実際の1秒間のフレーム数は均等ではない　フレーム数が違う機種によっての差をなくす 
+        }
+        else
+        {
+            float acceleratedZ = moveDirection.z + (accelerationZ * Time.deltaTime);//Time.deltaTime 0.0168
+            moveDirection.z = Mathf.Clamp(acceleratedZ, 0, speedZ);
+
+            float ratioX = (targetLane * LaneWidth - transform.position.x) / LaneWidth;//
+            moveDirection.x = ratioX * speedX;//speedX 設定した3　ねじこが左右に移動する速さ
+        }
+
+        moveDirection.y -= gravity * Time.deltaTime;//moveDirectionがマイナスになったらねじ子が落ちてくる
+        Vector3 globalDirection = transform.TransformDirection(moveDirection);//なくてもいい
+        controller.Move(globalDirection * Time.deltaTime);
+
+        if (controller.isGrounded)//地面に接触していたらTrue  isGroundedはCharacterControllerが持っているプロパティ
+        {
+            moveDirection.y = 0;
+        }
+        animator.SetBool("run", moveDirection.z > 0f);
+
+    }
+    public void MoveToLeft()
+    {
+        if (IsStun())
+        {
+            return;
+        }
+        if (controller.isGrounded && targetLane > MinLane)
+        {
+            targetLane--;
+        }
+    }
+    public void MoveToRight()
+    {
+        if (IsStun())
+        {
+            return;
+        }
+        if (controller.isGrounded && targetLane < MaxLane)
+        {
+            targetLane++;
+        }
+    }
+    public void Jump()
+    {
+        if (IsStun())
+        {
+            return;
+        }
+        if (controller.isGrounded)
+        {
+            moveDirection.y = speedJump;
+            animator.SetTrigger("jump");
+        }
+    }
+    void OnControllerColliderHit(ControllerColliderHit hit)//characterController で動かすか rigitbodyで動かすか　 characterControllerがあるオブジェクトと衝突した時の判定 OnTrrigerEnter OnCollitionEnter
+    {
+        if (IsStun())
+        {
+            return;
+        }
+        if(hit.gameObject.tag == "Robo")
+        {
+            life--;
+            recoverTime = StunDuration;
+
+            animator.SetTrigger("damage");
+
+            Destroy(hit.gameObject);
+        }
+    }
+}
+
+
+
+/*
+ 
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NejikoController : MonoBehaviour
+{
+    const int MinLane = -2;
+    const int MaxLane = 2;
+    const float LaneWidth = 1.0f;
+
+    CharacterController controller;
+    Animator animator;
+
+    Vector3 moveDirection = Vector3.zero;
+    int targetLane;
+
+    public float gravity;
+    public float speedZ;
+    public float speedX;
+    public float speedJump;
+    public float accelerationZ;
 
     // Start is called before the first frame update
     void Start()
@@ -23,6 +169,26 @@ public class NejikoController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown("left"))
+        {
+            MoveToLeft();
+        }
+        if (Input.GetKeyDown("right"))
+        {
+            MoveToRight();
+        }
+        if (Input.GetKeyDown("space"))
+        {
+            Jump();
+        }
+
+        float acceleratedZ = moveDirection.z + (accelerationZ * Time.deltaTime);
+        moveDirection.z = Mathf.Clamp(acceleratedZ, 0, speedZ);
+
+        float ratioX = (targetLane * LaneWidth - transform.position.x) / LaneWidth;
+        moveDirection.z = ratioX * speedX;
+
+        /*
         if (controller.isGrounded)//地面に接触していたらTrue  isGroundedはCharacterControllerが持っているプロパティ
         {
             if (Input.GetAxis("Vertical") > 0.0f)
@@ -42,6 +208,8 @@ public class NejikoController : MonoBehaviour
                 animator.SetTrigger("jump");
             }
         }
+        /
+
         moveDirection.y -= gravity * Time.deltaTime;//Time.deltaTime1フレームを描画するのにかかる時間 どの端末でも時間差が出ないようにするために必要
 
         Vector3 globalDirection = transform.TransformDirection(moveDirection);//方向を考慮した方向に進む
@@ -53,5 +221,31 @@ public class NejikoController : MonoBehaviour
         }
 
         animator.SetBool("run", moveDirection.z > 0.0f);//animator見た目のコンポーネント moveDirection.z > 0.0f 今正の方向にボタンが押されていればTrue
-    }
+            }
+
+                public void MoveToLeft()
+        {
+            if (controller.isGrounded && targetLane > MinLane)
+            {
+                targetLane--;
+            }
+        }
+        public void MoveToRight()
+        {
+            if (controller.isGrounded && targetLane < MaxLane)
+            {
+                targetLane++;
+            }
+        }
+        public void Jump()
+        {
+            if (controller.isGrounded)
+            {
+                moveDirection.y = speedJump;
+
+                animator.SetTrigger("Jump");
+            }
+       }
 }
+ 
+ */
---
+++ Assets/Scripts/StageGenerator.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageGenerator : MonoBehaviour
+{
+    const int StageChipSize = 30;
+
+    int currentChipIndex;//生成済みの最大インデックス
+
+    public Transform character;
+    public GameObject[] stageChips;
+    public int startChipIndex;//1
+    public int preInstantiate;//前方にいくつ作るか 5
+    public List<GameObject> generatedStageList = new List<GameObject>();//Listをnewしている
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentChipIndex = startChipIndex - 1;//1-1=0 生成済みは0
+        UpdateStage(preInstantiate);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int charaPositionIndex = (int)(character.position.z / StageChipSize);
+
+        if(charaPositionIndex + preInstantiate > currentChipIndex)
+        {
+            UpdateStage(charaPositionIndex + preInstantiate);
+        }
+    }
+
+    void UpdateStage(int toChipIndex)
+    {
+        if(toChipIndex <= currentChipIndex)//あってもなくても変わらない
+        {
+            return;
+        }
+        for(int i=currentChipIndex + 1; i <= toChipIndex; i++)
+        {
+            GameObject stageObject = GenerateStage(i);
+
+            generatedStageList.Add(stageObject);
+        }
+
+        while(generatedStageList.Count > preInstantiate + 2)
+        {
+            DestroyOldestStage();
+        }
+        currentChipIndex = toChipIndex;
+    }
+
+    GameObject GenerateStage(int chipIndex)
+    {
+        int nextStageChip = Random.Range(0, stageChips.Length);
+
+        GameObject stageObject = (GameObject)Instantiate(
+            stageChips[nextStageChip],
+            new Vector3(0,0,chipIndex * StageChipSize),//(0,0,30)
+            Quaternion.identity
+            );
+
+        return stageObject;
+    }
+
+    void DestroyOldestStage()
+    {
+        GameObject oldStage = generatedStageList[0];//住所取得
+        generatedStageList.RemoveAt(0);//インデックスを指定して消すのはRemoveAt
+        Destroy(oldStage);//ゲーム画面からも削除
+    }
+}
