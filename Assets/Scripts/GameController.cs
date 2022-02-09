6ab60089df49f2c88b089ad8993208e81edd6624
hogehoge <hogehoge@gmail.com>
2022-02-09 13:52:37 +0900

    p311まで作成

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;//UIはText型を認識するために必要
+
+public class GameController : MonoBehaviour
+{
+    public NejikoController nejiko;
+    public Text scoreText;
+    public LifePanel lifePanel;
+
+    // Update is called once per frame
+    void Update()
+    {
+        int score = CalcScore();
+        scoreText.text = $"Score:{score}m";//scoreText.fontSize = 30;
+        lifePanel.UpdateLife(nejiko.Life());//Component型だったら　lifePanel.UpdateLife(nejiko.gameObject.GetComponent<NejikoController>().Life())
+        //GameOblect型だったら lifePanel.UpdateLife(nejiko.GetComponent<NejikoController>().Life())
+        //transformは特殊 .transformができる
+    }
+
+    int CalcScore()
+    {
+        return (int)nejiko.transform.position.z;//.gameObject.GetComponent～～  ～～はanimatorやcharacterなど transformは特殊だから.transformでできる
+    }
+}
