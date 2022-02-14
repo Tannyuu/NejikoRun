4403798553b8448e3ee8b5add3990f1d51d493bb
hogehoge <hogehoge@gmail.com>
2022-02-14 11:01:43 +0900

    NejikoRun完成

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;//UIはText型を認識するために必要
+using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
@@ -17,10 +18,27 @@ public class GameController : MonoBehaviour
         lifePanel.UpdateLife(nejiko.Life());//Component型だったら　lifePanel.UpdateLife(nejiko.gameObject.GetComponent<NejikoController>().Life())
         //GameOblect型だったら lifePanel.UpdateLife(nejiko.GetComponent<NejikoController>().Life())
         //transformは特殊 .transformができる
+
+        if (nejiko.Life() <= 0)
+        {
+            enabled = false;//GameControllerのコンポーネントをオフにできる プロパティ
+
+            if (PlayerPrefs.GetInt("HightScore") < score)
+            {
+                PlayerPrefs.SetInt("HighScore", score);
+            }
+
+            Invoke("ReturnToTitle", 2.0f);//指定時間後にReturnToTitleを呼ぶ　引数が必要なものは呼べない
+        }
     }
 
     int CalcScore()
     {
         return (int)nejiko.transform.position.z;//.gameObject.GetComponent～～  ～～はanimatorやcharacterなど transformは特殊だから.transformでできる
+    }
+
+    void ReturnToTitle()
+    {
+        SceneManager.LoadScene("Title");
     }
 }
